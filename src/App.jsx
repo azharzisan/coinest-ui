46ee0288a5921ed6157acd9fcae25d26c726743d
Azhar Zisan <azharzisanxtr@gmail.com>
2026-07-25 22:11:50 +0100

    sections added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import Dash from "./sections/Dash";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
+import Services from "./sections/Services";
+import Sessions from "./sections/Sessions";
+import Backbone from "./sections/Backbone";
 
 function App() {
 
@@ -12,6 +16,10 @@ function App() {
 
         <section className="">
           <Hero />
+          <Services />
+          <Dash />
+          <Sessions />
+          <Backbone />
         </section>
       </section>
     </>
