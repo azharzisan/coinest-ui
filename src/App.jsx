eb0f7c76c9fe576be273ee27f0b643649f56523b
Azhar Zisan <azharzisanxtr@gmail.com>
2026-07-28 21:06:07 +0100

    new project loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
-import Dash from "./sections/Dash";
-import Hero from "./sections/Hero";
-import Navbar from "./sections/Navbar";
-import Services from "./sections/Services";
-import Sessions from "./sections/Sessions";
-import Backbone from "./sections/Backbone";
-import Earth from "./sections/Earth";
+
 
 function App() {
 
   return (
     <>
-      <section>
-        <nav>
-          <Navbar />
-        </nav>
-
-        <section className="">
-          <Hero />
-          <Services />
-          <Dash />
-          <Sessions />
-          <Backbone />
-          <Earth />
-        </section>
-      </section>
+      
     </>
   );
 }
