fadc5bac291cdcbd58e9db7b481a4b54b640a944
Azhar Zisan <azharzisanxtr@gmail.com>
2026-08-06 22:16:11 +0100

    shadcn utilities

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,21 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss()
+    tailwindcss(),
   ],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: true,
+  },
+});
