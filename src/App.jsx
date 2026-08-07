101f6bb3f0c35b901f323881be5cddabc386230c
Azhar Zisan <azharzisanxtr@gmail.com>
2026-08-07 22:31:49 +0100

    structure and shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Navbar from "./sections/Navbar";
 import Homepage from "./sections/Homepage";
+import LogoRun from "./sections/LogoRun";
+import Features from "./sections/Features";
+import Core from "./sections/Core";
 
 const App = () => {
   return (
     <>
-      <section className="w-full py-4 px-15 fixed">
+      <section className="w-full py-4 px-15 fixed z-10000">
         <Navbar />
       </section>
       <section className="w-full pt-30">
         <Homepage />
+        <LogoRun />
+        <Features />
+        <Core />
       </section>
     </>
   );
