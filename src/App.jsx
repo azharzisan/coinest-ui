2fe53c3211fe36abab4383900a27ea50a6c54d0a
Azhar Zisan <azharzisanxtr@gmail.com>
2026-08-08 23:10:34 +0100

    section addeD

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Navbar from "./sections/Navbar";
 import Homepage from "./sections/Homepage";
 import LogoRun from "./sections/LogoRun";
 import Features from "./sections/Features";
 import Core from "./sections/Core";
+import Overview from "./sections/Overview";
+import WhyUs from "./sections/WhyUs";
+import Integration from "./sections/Integration";
+import Review from "./sections/Review";
 
 const App = () => {
+  // const cursorRef = useRef(null)
+  // useEffect(() => {
+  //   const handleMouseMove = (e) => {
+  //     if (cursorRef.current) {
+  //       cursorRef.current.style.left = `${e.clientX}px`;
+  //       cursorRef.current.style.top = `${e.clientY}px`;
+  //     }
+  //   }
+  //   window.addEventListener("mousemove", handleMouseMove);
+  
+  //   return () => {
+  //     window.removeEventListener("mousemove", handleMouseMove);
+  //   }
+  // }, [])
+  
   return (
     <>
+      {/* <div ref={cursorRef} class="custom-cursor w-5 h-5 bg-primary fixed rounded-full -translate-x-1/2 -translate-y-1/2 pointer-events-none transition-all duration-100 ease-in-out z-1000000"></div> */}
       <section className="w-full py-4 px-15 fixed z-10000">
         <Navbar />
       </section>
@@ -16,6 +36,10 @@ const App = () => {
         <LogoRun />
         <Features />
         <Core />
+        <Overview />
+        <WhyUs />
+        <Integration />
+        <Review />
       </section>
     </>
   );
