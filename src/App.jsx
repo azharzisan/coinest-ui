1196afb2426eafa6bddbacc5415a1a12c788fb05
Azhar Zisan <azharzisanxtr@gmail.com>
2026-08-09 15:42:07 +0100

    sections added

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,15 @@ import Overview from "./sections/Overview";
 import WhyUs from "./sections/WhyUs";
 import Integration from "./sections/Integration";
 import Review from "./sections/Review";
+import Pricing from "./sections/Pricing";
+import Sessions from "./sections/Sessions";
+import Ready from "./sections/Ready";
+import Footer from "./sections/Footer";
 
 const App = () => {
-  // const cursorRef = useRef(null)
-  // useEffect(() => {
-  //   const handleMouseMove = (e) => {
-  //     if (cursorRef.current) {
-  //       cursorRef.current.style.left = `${e.clientX}px`;
-  //       cursorRef.current.style.top = `${e.clientY}px`;
-  //     }
-  //   }
-  //   window.addEventListener("mousemove", handleMouseMove);
-  
-  //   return () => {
-  //     window.removeEventListener("mousemove", handleMouseMove);
-  //   }
-  // }, [])
   
   return (
     <>
-      {/* <div ref={cursorRef} class="custom-cursor w-5 h-5 bg-primary fixed rounded-full -translate-x-1/2 -translate-y-1/2 pointer-events-none transition-all duration-100 ease-in-out z-1000000"></div> */}
       <section className="w-full py-4 px-15 fixed z-10000">
         <Navbar />
       </section>
@@ -40,6 +29,10 @@ const App = () => {
         <WhyUs />
         <Integration />
         <Review />
+        <Pricing />
+        <Sessions />
+        <Ready />
+        <Footer />
       </section>
     </>
   );
