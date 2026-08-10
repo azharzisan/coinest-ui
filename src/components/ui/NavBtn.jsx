64b9ef39f9e3365f2bc54ad45a01e8828b44477a
Azhar Zisan <azharzisanxtr@gmail.com>
2026-08-10 14:45:24 +0100

    anvhor tag with props added

--- src/components/ui/NavBtn.jsx
+++ src/components/ui/NavBtn.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const NavBtn = ({text}) => {
+const NavBtn = ({text, ref}) => {
   return (
     <button className="text-sm lg:text-md text-muted px-4 rounded-xl py-2 hover:bg-secondary/40 hover:text-primary font-semibold active:translate-y-1 transition-all duration-500 ease-in-out">
-      {text}
+      <a href={`#${ref}`}>{text}</a>
     </button>
   );
 }
